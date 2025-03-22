67d29ef0d52e1520bdd10f07897b4e7895ba2605
Shana Sherfudheen <shanasherfudheen@gmail.com>
2025-03-22 08:24:23 -0800

    Created a Lead Tracker Chrome extension

--- index.js
+++ index.js
@@ -2,22 +2,49 @@ let myLeads = [];
 const inputEL = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEL.value);
-  inputEL.value = "";
-  renderLeads();
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    console.log(tabs[0].url);
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
 });
 
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li>
-      <a href = ${myLeads[i]} target = "_blank">${myLeads[i]}</a>
+      <a href = ${leads[i]} target = "_blank">${leads[i]}</a>
     </li>`;
     console.log(listItems);
   }
 
   ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEL.value);
+  inputEL.value = "";
+  //save leads to local storage
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+  console.log(localStorage.getItem("myLeads"));
+});
