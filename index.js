4557fb587e753dafffc98d4ce9e63f45e2e38ab4
Shana Sherfudheen <shanasherfudheen@gmail.com>
2025-03-24 11:17:08 -0800

    Leads Tracker App

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-let myLeads = [];
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/11.5.0/firebase-database.js";
+
+const firebaseConfig = {
+  databaseURL:
+    "https://leads-tracker-app-a5f24-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const referenceInDB = ref(database, "leads");
+
 const inputEL = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
-const tabBtn = document.getElementById("tab-btn");
-
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage;
-  render(myLeads);
-}
-
-tabBtn.addEventListener("click", function () {
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    console.log(tabs[0].url);
-    myLeads.push(tabs[0].url);
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    render(myLeads);
-  });
-});
 
 function render(leads) {
   let listItems = "";
@@ -34,17 +34,21 @@ function render(leads) {
   ulEl.innerHTML = listItems;
 }
 
+onValue(referenceInDB, function (snapshot) {
+  const snapshotDoesExist = snapshot.exists();
+  if (snapshotDoesExist) {
+    const snapshotValues = snapshot.val();
+    const leads = Object.values(snapshotValues);
+    render(leads);
+  }
+});
+
 deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear();
-  myLeads = [];
-  render(myLeads);
+  remove(referenceInDB);
+  ulEl.innerHTML = "";
 });
 
 inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEL.value);
+  push(referenceInDB, inputEL.value);
   inputEL.value = "";
-  //save leads to local storage
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  render(myLeads);
-  console.log(localStorage.getItem("myLeads"));
 });
